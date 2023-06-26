146ffe7edc9c6f119fc80144124a96f316f7e356
Prabhat Roy <prabhat00155@gmail.com>
2023-06-26 16:25:10 +0100

    Add more metadata to apps and games

--- games.py
+++ games.py
@@ -22,9 +22,28 @@ def fetch(games):
         current['background_image'] = game['background_image']
         current['metacritic'] = game['metacritic']
         current['rating'] = game['rating']
+        current['released'] = game['released']
+        current['playtime'] = game['playtime']
         current['ratings_count'] = game['ratings_count']
         current['esrb_rating'] = game['esrb_rating']['name'] if game['esrb_rating'] else None
         current['genres'] = list(map(lambda x: x['name'], game['genres']))
         current['locations'] = locations 
+        game_id = game['id']
+        current['description'] = ''
+        current['website'] = ''
+        current['reddit_url'] = ''
+        current['metacritic_url'] = ''
+
+        url = f"https://api.rawg.io/api/games/{game_id}"
+        params = { 'key': API_KEY }
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+          data = response.json()
+          if data:
+            current['description'] = data['description']
+            current['website'] = data['website']
+            current['reddit_url'] = data['reddit_url']
+            current['metacritic_url'] = data['metacritic_url']
+
         game_list.append(current)
   return game_list
